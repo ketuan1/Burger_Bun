87aff32860cad6d531758638f5fc7e1e7959adcb
Đặng Thanh Tuấn <91366633+ketuan1@users.noreply.github.com>
2023-02-18 19:04:02 +0700

    feature component

---
+++ src/components/Feature/feature.tsx
@@ -0,0 +1,96 @@
+function Feature() {
+    return ( 
+        <>
+            <section className="w3l-mobile-content-6 py-5">
+    <div className="mobile-info py-lg-5 py-md-4 py-2">
+        <div className="container">
+            <div className="row mobile-info-inn mx-lg-0">
+                <div className="col-lg-4 mobile-right">
+                    <div className="row mobile-right-grids mb-lg-5 mb-4">
+                        <div className="col-2 mobile-right-icon">
+                            <div className="mobile-icon">
+                                <span className="fa fa-leaf"></span>
+                            </div>
+                        </div>
+                        <div className="col-10 mobile-right-info">
+                            <h6><a href="#url">Natural ingredients</a></h6>
+                            <p>Lorem ipsum dolor sit amet,Ea sed illum facere aperiam sequi optio consec
+                                adipisicing elit.</p>
+                        </div>
+                    </div>
+                    <div className="row mobile-right-grids mb-lg-5 mb-4">
+                        <div className="col-2 mobile-right-icon">
+                            <div className="mobile-icon">
+                                <span className="fa fa-shopping-basket"></span>
+                            </div>
+                        </div>
+                        <div className="col-10 mobile-right-info">
+                            <h6><a href="#url"> Fresh vegetables & Meet</a></h6>
+                            <p>Lorem ipsum dolor sit amet,Ea sed illum facere aperiam sequi optio consec
+                                adipisicing elit.</p>
+                        </div>
+                    </div>
+                    <div className="row mobile-right-grids">
+                        <div className="col-2 mobile-right-icon">
+                            <div className="mobile-icon">
+                                <span className="fa fa-users"></span>
+                            </div>
+                        </div>
+                        <div className="col-10 mobile-right-info">
+                            <h6><a href="#url">World’s best Chef </a></h6>
+                            <p>Lorem ipsum dolor sit amet,Ea sed illum facere aperiam sequi optio consec
+                                adipisicing elit.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-4 mobile-left">
+                    <img src="assets/images/burger1.png" className="img-fluid radius-image" alt="" />
+                </div>
+                <div className="col-lg-4 mobile-right">
+                    <div className="row mobile-right-grids mb-lg-5 mb-4">
+                        <div className="col-2 mobile-right-icon">
+                            <div className="mobile-icon">
+                                <span className="fa fa-cogs"></span>
+                            </div>
+                        </div>
+                        <div className="col-10 mobile-right-info">
+                            <h6><a href="#url">Best quality products</a></h6>
+                            <p>Lorem ipsum dolor sit amet,Ea sed illum facere aperiam sequi optio consec
+                                adipisicing elit.</p>
+                        </div>
+                    </div>
+                    <div className="row mobile-right-grids mb-lg-5 mb-4">
+                        <div className="col-2 mobile-right-icon">
+                            <div className="mobile-icon">
+                                <span className="fa fa-motorcycle"></span>
+                            </div>
+                        </div>
+                        <div className="col-10 mobile-right-info">
+                            <h6><a href="#url">Fastest door delivery</a></h6>
+                            <p>Lorem ipsum dolor sit amet,Ea sed illum facere aperiam sequi optio consec
+                                adipisicing elit.</p>
+                        </div>
+                    </div>
+                    <div className="row mobile-right-grids">
+                        <div className="col-2 mobile-right-icon">
+                            <div className="mobile-icon">
+                                <span className="fa fa-thumbs-down"></span>
+                            </div>
+                        </div>
+                        <div className="col-10 mobile-right-info">
+                            <h6><a href="#url">Ground beef & Low fat</a></h6>
+                            <p>Lorem ipsum dolor sit amet,Ea sed illum facere aperiam sequi optio consec
+                                adipisicing elit.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+        </>
+     );
+}
+
+export default Feature;
--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import logo from '../../image/lg.png';
 import CategoryProduct from '../CategoryProduct/categoryProduct';
+import Feature from '../Feature/feature';
 import Footer from '../Footer/footer';
 import '../Home/home.css';
 import Navigator from '../Navigator/navigator';
@@ -88,96 +89,12 @@ function Home() {
     </div>
 </section>
 
+     {/* components */}
         <CategoryProduct />
 
-<section className="w3l-mobile-content-6 py-5">
-    <div className="mobile-info py-lg-5 py-md-4 py-2">
-        <div className="container">
-            <div className="row mobile-info-inn mx-lg-0">
-                <div className="col-lg-4 mobile-right">
-                    <div className="row mobile-right-grids mb-lg-5 mb-4">
-                        <div className="col-2 mobile-right-icon">
-                            <div className="mobile-icon">
-                                <span className="fa fa-leaf"></span>
-                            </div>
-                        </div>
-                        <div className="col-10 mobile-right-info">
-                            <h6><a href="#url">Natural ingredients</a></h6>
-                            <p>Lorem ipsum dolor sit amet,Ea sed illum facere aperiam sequi optio consec
-                                adipisicing elit.</p>
-                        </div>
-                    </div>
-                    <div className="row mobile-right-grids mb-lg-5 mb-4">
-                        <div className="col-2 mobile-right-icon">
-                            <div className="mobile-icon">
-                                <span className="fa fa-shopping-basket"></span>
-                            </div>
-                        </div>
-                        <div className="col-10 mobile-right-info">
-                            <h6><a href="#url"> Fresh vegetables & Meet</a></h6>
-                            <p>Lorem ipsum dolor sit amet,Ea sed illum facere aperiam sequi optio consec
-                                adipisicing elit.</p>
-                        </div>
-                    </div>
-                    <div className="row mobile-right-grids">
-                        <div className="col-2 mobile-right-icon">
-                            <div className="mobile-icon">
-                                <span className="fa fa-users"></span>
-                            </div>
-                        </div>
-                        <div className="col-10 mobile-right-info">
-                            <h6><a href="#url">World’s best Chef </a></h6>
-                            <p>Lorem ipsum dolor sit amet,Ea sed illum facere aperiam sequi optio consec
-                                adipisicing elit.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-lg-4 mobile-left">
-                    <img src="assets/images/burger1.png" className="img-fluid radius-image" alt="" />
-                </div>
-                <div className="col-lg-4 mobile-right">
-                    <div className="row mobile-right-grids mb-lg-5 mb-4">
-                        <div className="col-2 mobile-right-icon">
-                            <div className="mobile-icon">
-                                <span className="fa fa-cogs"></span>
-                            </div>
-                        </div>
-                        <div className="col-10 mobile-right-info">
-                            <h6><a href="#url">Best quality products</a></h6>
-                            <p>Lorem ipsum dolor sit amet,Ea sed illum facere aperiam sequi optio consec
-                                adipisicing elit.</p>
-                        </div>
-                    </div>
-                    <div className="row mobile-right-grids mb-lg-5 mb-4">
-                        <div className="col-2 mobile-right-icon">
-                            <div className="mobile-icon">
-                                <span className="fa fa-motorcycle"></span>
-                            </div>
-                        </div>
-                        <div className="col-10 mobile-right-info">
-                            <h6><a href="#url">Fastest door delivery</a></h6>
-                            <p>Lorem ipsum dolor sit amet,Ea sed illum facere aperiam sequi optio consec
-                                adipisicing elit.</p>
-                        </div>
-                    </div>
-                    <div className="row mobile-right-grids">
-                        <div className="col-2 mobile-right-icon">
-                            <div className="mobile-icon">
-                                <span className="fa fa-thumbs-down"></span>
-                            </div>
-                        </div>
-                        <div className="col-10 mobile-right-info">
-                            <h6><a href="#url">Ground beef & Low fat</a></h6>
-                            <p>Lorem ipsum dolor sit amet,Ea sed illum facere aperiam sequi optio consec
-                                adipisicing elit.</p>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
-</section>
+     {/* components */}
+      <Feature />
+     
 <div className="middle py-5" id="call">
     <div className="container py-lg-3">
         <div className="welcome-left text-center py-md-5 py-3">
